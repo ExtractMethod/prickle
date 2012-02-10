628e9330e9eccb560c467f4276687a1448a16332
Despo Pentara <despo@despo.me>
2012-02-10 22:20:53 +0000

    extract text highlight to method

--- lib/prickle/capybara/exceptions.rb
+++ lib/prickle/capybara/exceptions.rb
@@ -4,12 +4,15 @@ module Prickle
 
       def initialize type, identifier, text, caught_exception
         element_text = "Element"
-        element_text = "\e[1m#{type}\e[0m\e[31m" unless type == "*"
-        text_string = " and text \e[1m#{text}\e[0m\e[31m" unless text.nil?
+        element_text = hightlight(type) unless type == "*"
+        text_string = " and text #{highlight(text)}" unless text.nil?
 
-        raise "#{element_text} with properties \e[1m#{identifier.to_s}\e[0m\e[31m#{text_string} was not found.\n\tError: #{caught_exception.message}"
+        raise "#{element_text} with properties #{highlight(identifier.to_s)} #{text_string} was not found.\n\tError: #{caught_exception.message}"
       end
 
+      def highlight text
+        "\e[1m#{text}\e[0m\e[31m"
+      end
     end
 
   end
