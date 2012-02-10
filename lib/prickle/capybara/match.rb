b0429871e4cefc5f187b232e8ed5e477fc888007
Despo Pentara <despo@despo.me>
2012-02-10 22:22:27 +0000

    use identifier instead of name

--- lib/prickle/capybara/match.rb
+++ lib/prickle/capybara/match.rb
@@ -4,7 +4,7 @@ module Prickle
 
       def contains_text? text
         handle_exception do
-          find_element_by_name_and_text(@type, @identifier, text)
+          find_element_by_identifier_and_text(@type, @identifier, text)
         end
       end
 
@@ -12,20 +12,21 @@ module Prickle
 
       def method_missing method, *args
         if method =~ /(^.*)_contains_text\?$/
-          find_element_by_name_and_text($1,  { :name => args.first } , args[1])
+          find_element_by_identifier_and_text($1,  { :name => args.first } , args[1])
         else
           super
         end
       end
 
 
-      def find_by_name_and_text_xpath element, identifier, text
+      def find_by_identifier_and_text_xpath element, identifier, text
         "//#{type_of(element)}[#{xpath_for(identifier)} and contains(text(), '#{text}')]"
       end
 
-      def find_element_by_name_and_text element, name, text
+      def find_element_by_identifier_and_text element, identifier, text
+        @text = text
         handle_exception do
-          find_element_by(find_by_name_and_text_xpath(element, name, text))
+          find_element_by(find_by_identifier_and_text_xpath(element, identifier, text))
         end
       end
 
