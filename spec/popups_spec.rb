67a694b6bc1adca1caa9455b3b7059def94583ec
Despo Pentara <despo@despo.me>
2012-02-11 03:06:30 +0000

    removing @wip

--- spec/popups_spec.rb
+++ spec/popups_spec.rb
@@ -14,17 +14,17 @@ describe Prickle::Capybara do
       prickly.visit '/'
     end
 
-    it 'can confirm an alert box', :wip => true do
+    it 'can confirm an alert box' do
       prickly.click_by_name 'popups'
       prickly.confirm_popup
     end
 
-    it 'can confirm a popup', :wip => true do
+    it 'can confirm a popup' do
       prickly.click_by_name 'confirm_box'
       prickly.confirm_popup
     end
 
-    it 'can dismiss a popup', :wip => true do
+    it 'can dismiss a popup' do
       prickly.click_by_name 'confirm_box'
       prickly.dismiss_popup
       prickly.popup_message.should eq "Aborting."
