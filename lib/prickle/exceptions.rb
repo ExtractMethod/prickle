14c414c7de9593f461f8d2c25c2276f8cb7ec0b0
Despo Pentara <despo@despo.me>
2012-02-12 01:30:47 +0000

    moving exception messages to their own file

--- lib/prickle/exceptions.rb
+++ lib/prickle/exceptions.rb
@@ -1,79 +1,26 @@
+require_relative 'exceptions/messages'
+
 module Prickle
   module Exceptions
 
+    def message
+      @message
+    end
+
     class ElementNotFound < Exception
+      include Prickle::Exceptions
 
       def initialize(type, identifier, text, caught_exception)
         @message = Message::ElementNotFound.new(type, identifier, text, caught_exception).to_s
       end
-
-      def message
-        @message
-      end
     end
 
     class MessageNotContainedInPopup < Exception
+      include Prickle::Exceptions
 
       def initialize message
         @message = Message::TextNotContainedInPopup.new(message).to_s
       end
-
-      def message
-        @message
-      end
     end
-
-
-    module Message
-
-      private
-      def highlight text
-        "\e[1m#{text}\e[0m\e[31m"
-      end
-
-      class ElementNotFound
-        include Message
-
-        def initialize type=nil, identifier=nil, text=nil, caught_exception=nil
-          @element_type = type
-          @element_identifier = identifier
-          @element_text = text
-          @caught_exception = caught_exception
-        end
-
-        def to_s
-          "#{element_text} with properties #{identifier} #{text_string} was not found.\n\tError: #{@caught_exception.message}"
-        end
-
-        private
-
-        def element_text
-          return highlight(@element_type) unless @element_type == "*"
-          "Element"
-        end
-
-        def text_string
-          " and text #{highlight(@element_text)}" unless @element_text.nil?
-        end
-
-        def identifier
-          highlight(@element_identifier.to_s)
-        end
-      end
-
-      class TextNotContainedInPopup
-        include Message
-
-        def initialize message
-          @message = message
-        end
-
-        def to_s
-          "Text #{highlight(@message)} is not contained in the popup."
-        end
-      end
-
-    end
-
   end
 end
---
+++ lib/prickle/exceptions/messages.rb
@@ -0,0 +1,55 @@
+module Prickle
+  module Exceptions
+    module Message
+
+      private
+      def highlight text
+        "\e[1m#{text}\e[0m\e[31m"
+      end
+
+      class ElementNotFound
+        include Message
+
+        def initialize type=nil, identifier=nil, text=nil, caught_exception=nil
+          @element_type = type
+          @element_identifier = identifier
+          @element_text = text
+          @caught_exception = caught_exception
+        end
+
+        def to_s
+          "#{element_text} with properties #{identifier} #{text_string} was not found.\n\tError: #{@caught_exception.message}"
+        end
+
+        private
+
+        def element_text
+          return highlight(@element_type) unless @element_type == "*"
+          "Element"
+        end
+
+        def text_string
+          " and text #{highlight(@element_text)}" unless @element_text.nil?
+        end
+
+        def identifier
+          highlight(@element_identifier.to_s)
+        end
+      end
+
+      class TextNotContainedInPopup
+        include Message
+
+        def initialize message
+          @message = message
+        end
+
+        def to_s
+          "Text #{highlight(@message)} is not contained in the popup."
+        end
+      end
+
+    end
+
+  end
+end
