9080d32aef0501aec5ec7156614db9e72bb37438
Despo Pentara <despo@despo.me>
2012-02-26 19:05:23 +0000

    rename @popup to @base

--- lib/prickle/capybara/popup.rb
+++ lib/prickle/capybara/popup.rb
@@ -6,17 +6,18 @@ module Prickle
     class Popup
 
       def initialize
-        return @popup = Popups::Webkit.new if ::Capybara::current_driver == :webkit
-        @popup = Popups::Selenium.new
+        return @base = Popups::Webkit.new if ::Capybara::current_driver == :webkit
+        @base = Popups::Selenium.new
       end
 
       def method_missing method, *args, &block
-        if @popup.respond_to? method
-          @popup.send method, *args, &block
+        if @base.respond_to? method
+          @base.send method, *args, &block
         else
           super
         end
       end
+
     end
   end
 end
