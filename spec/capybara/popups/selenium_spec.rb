9281833af5d3c32006a8263529ffbcf748ab81d1
Despo Pentara <despo@Despos-MacBook-Pro.local>
2012-07-13 00:01:30 +0100

    aiming for consistent usage between selenium and webkit

--- spec/capybara/popups/selenium_spec.rb
+++ spec/capybara/popups/selenium_spec.rb
@@ -38,4 +38,33 @@ describe Prickle::Capybara::Popup do
       end
     end
   end
+
+
+  context "alternate popup syntax", :javascript => true do
+    it 'can confirm an alert box', :wip => true do
+
+      alert = prickly.popup.accept {
+        prickly.click_by_name 'popups'
+      }
+
+      alert.contains_message? "Hello"
+    end
+
+    it 'can confirm a popup' do
+      alert = prickly.popup.confirm {
+        prickly.click_by_name 'confirm_box'
+      }
+      alert.contains_message? "Click yes to continue"
+    end
+
+    it 'can dismiss a popup' do
+      alert = prickly.popup.dismiss {
+        prickly.click_by_name 'confirm_box'
+      }
+
+      alert.contains_message? "Click yes to continue"
+    end
+
+  end
+
 end
